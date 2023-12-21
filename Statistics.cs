f4af8656c324ccf2fd997282caac3fc7acbfb75a
alexmangushev <mangushev2001@yandex.ru>
2023-12-21 10:26:15 +0300

    Refactor sort

--- Statistics.cs
+++ Statistics.cs
@@ -46,7 +46,9 @@ namespace mqtt_client
             List<double> values = new List<double>();
             List<DateTime> dates = new List<DateTime>();
 
-            foreach (var i in _listData)
+            var listDataDateSort = _listData.OrderBy(p => p.GetTime);
+
+            foreach (var i in listDataDateSort)
             {
                 dates.Add(i.GetTime);
 
@@ -71,24 +73,6 @@ namespace mqtt_client
                     case "Наличие дыма":
                         values.Add(i.Smoke ? 1 : 0);
                         break;
-                }
-            }
-
-            for (int i = 0; i < dates.Count() - 1; i++)
-            {
-                for (int k = 0; k < dates.Count() - 1; k++)
-                {
-                    
-                    if (DateTime.Compare(dates[k], dates[k + 1]) > 0)
-                    {
-                        DateTime tmp = dates[k];
-                        dates[k] = dates[k + 1];
-                        dates[k + 1] = tmp;
-
-                        double tmpD = values[k];
-                        values[k] = values[k + 1];
-                        values[k + 1] = tmpD;
-                    }
                 }
             }
 
